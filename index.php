46a282a91f6444242106e4f17af511398acd63a1
Flavien NGIMBI <flashnging@gmail.com>
2021-03-30 23:32:13 +0200

    Initialisation du projet

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    include "includes/menu.php";
+?>
+            <div class="col-lg-6 col-md-6 col-xs-12 section">
+                <img style="margin: 0" src="images/accueil6.jpg" alt="" srcset="">
+                <p>
+                    La Société KinDev est un bureau d’étude et un prestataire de services. En qualité de menbre d’un consortium qui réalise ses missions ponctuelles à travers le pays. KinDev travaille en étroite collaboration avec ses partenaires nationaux et  internationaux dans le but d’apporter une valeur ajoutée à vos produits et assurer un travail nécessaire aux particuliers, enterprises  et industries.
+                </p>
+                
+                <div class="sous-section">
+                    <img src="images/accueil8.jpg" alt="" srcset="">
+                    <img src="images/accueil9.jpg" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-xs-12 footers">
+
+            </div>
+            <div class="col-lg-3 col-md-3 col-xs-12 footers">
+
+            </div>
+            <div class="col-lg-3 col-md-3 col-xs-12 footers">
+
+            </div>
+        </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
